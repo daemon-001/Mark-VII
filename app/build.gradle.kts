cd5c33814090aff3ba97e6126f62b3546ad47052
Nitesh Kumar <niteshk151617@gmail.com>
2025-12-07 05:14:52 +0530

    Bump app version to 3.1.0 (versionCode 30100)
    Updated versionCode and versionName in build.gradle.kts to reflect the new release version 3.1.0. This prepares the app for the next deployment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.daemon.markvii"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 30100
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
